d35100cca34768f3caf733e079ddd87d8288bdb5
Rashidulhaq <rashidulhaq015@gmail.com>
2023-03-01 02:13:10 +0600

    first commit

---
+++ lms/admin/remove_student.php
@@ -0,0 +1,4 @@
+
+<button type="button" name="delete_button" class="btn btn-danger btn-sm"
+                                            oneclick="delete_data(`'.$row["name"].'`,`'.$row["rollno"].'`,`'.$row['email'].'
+                                            `)">Delete</button>
